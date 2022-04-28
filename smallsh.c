cb0b668a13e2197540caae22be77944f4fdddca6
Alyssa Comstock <alyssacomstock95@gmail.com>
2022-04-27 22:45:22 -0700

    pipe in from stdout is working

--- smallsh.c
+++ smallsh.c
@@ -11,8 +11,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <sys/wait.h>
 #include <unistd.h>
+
 
 
 /* CONSTANTS */
@@ -22,8 +25,8 @@
 
 struct userArgs{
 	char * args[MAX_ARG_NUM]; // contains the args
-	char *infile; // contains the in file supplied by the user
-	char *outfile; // contains the outfile supplied by the user
+	char infile[256]; // contains the in file supplied by the user
+	char outfile[256]; // contains the outfile supplied by the user
 	int background; // & symbol at end == execute in background, so background = 1. Else background = 0
 	int amount_args; // For deallocation
 };
@@ -50,9 +53,9 @@ int main(){
 	// init child stuff	
 	int childStatus = 0;
 	//int childPid;
-	pid_t spawnpid = -5;	
+	pid_t childPid = -5;	
 	
-
+	/* TODO: GET ECHO WORKING BEFORE TRYING TO RUN THE TEST SCRIPT*/
 
 	do{	
 		// get user input from stdin
@@ -68,7 +71,7 @@ int main(){
 		addArgsToStruct(user_args, &Args, amount);		
 	
 		// testing display
-		displayArgs(&Args);
+		//displayArgs(&Args);
 
 		userArgsCleanUp(user_args, amount);
 		free(userInput);
@@ -80,26 +83,20 @@ int main(){
 			do{
 				z = wait(NULL);
 				fprintf(stdout, "Freed: %d\n", z);
+				fflush(stdout);
 			}while(z != -1);		
 	
 			// memory clean up before returning
-			userArgsCleanUp(Args.args, Args.amount_args);
+			//userArgsCleanUp(Args.args, Args.amount_args);
 
-			if(Args.infile){
-				free(Args.infile);
-			}
-			if(Args.outfile){
-				free(Args.outfile);
-			}
+			for(int i=0; i < Args.amount_args; ++i) free(Args.args[i]);
 			// we are done here!
 			exit(0);
 		}else if(strcmp(Args.args[0], "cd") == 0){
 			// cd to user defined dir in the command
 			fprintf(stdout, "cd to dir: %s\n", Args.args[1]);
 			fflush(stdout);
-			/* TODO: 
- 			*  - MAKE SURE THAT ABSOLUTE AND RELATIVE PATHS BOTH WORK
- 			*/
+	
 			cdAndUpdatePWD(Args.args[1]);
 				
 		}else if(strcmp(Args.args[0], "status") == 0 || strcmp(Args.args[0], "status\n") == 0){
@@ -109,22 +106,89 @@ int main(){
 			fflush(stdout);
 		}else{
 			// handle other args here
-			fprintf(stdout, "other args\n");
-			fflush(stdout);
-			spawnpid = fork();
+			//fprintf(stdout, "other args\n");
+			//fflush(stdout);
+			childPid = fork();
 			
-			switch(spawnpid){
+			// TODO: REMOVE LATER
+			// testing: automatically kill process after 10 min
+			alarm(600);		
+	
+			switch(childPid){
 				case -1:
 					fprintf(stderr, "Failed to fork.\n");
-					fflush(stderr);
+					fflush(stderr);	
+					for(int i=0; i < Args.amount_args; ++i) free(Args.args[i]);
+
 					exit(1);
 					break;
 				case 0:
 					// child code
-					exit(0);
+					//fprintf(stdout,"Child successfully spawned\n");
+					//fflush(stdout);
+						
+					if(Args.infile[0] != '\0'){
+						/* Citation for use of dup2 to read in stdin output into infile
+ 						 * Author: Michael Kerrisk
+ 						 * Book: The Linus Programming Interface
+ 						 * Chapter: 27.4 - File Descriptors and exec()
+ 						 * Page: 576
+ 						 * Date: 4/27/2022
+ 						 * Adapted frome example code on use of dup2
+ 						 * */
+						// open in file, get contents from execvp, put it in infile.
+						int fd = open(Args.infile, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+						if(fd == -1){
+							// error
+							fprintf(stderr, "Cannot open %s for input\n", Args.infile);
+							fflush(stderr);
+						}
+						fflush(stdout);
+						if(fd!=STDOUT_FILENO){
+							dup2(fd, STDOUT_FILENO);
+						}
+						close(fd);
+					}
+
+					if(Args.outfile[0] != '\0'){
+						/* Citation for use of dup2 to read in stdin output into infile
+ 						* Author: Michael Kerrisk
+ 						* Book: The Linus Programming Interface
+ 						* Chapter: 27.4 - File Descriptors and exec()
+ 						* Page: 576
+ 						* Date: 4/27/2022
+ 						* Adapted frome example code on use of dup2
+ 						* */
+
+						// if there is an outfile
+						//fprintf("outfile\n");
+						//fflush(stdout);
+					}
+
+					
+					// execute command	
+					execvp(Args.args[0], Args.args);
+					
+					fprintf(stderr, "Error with cmd\n");
+					fflush(stderr);
+					// mem clean up
+					for(int i=0; i < Args.amount_args; ++i) free(Args.args[i]);
+
+					exit(2);
 					break;
 				default:
-					wait(&childStatus);
+				
+					fflush(stdout);	
+					// wait for child to come back
+					childPid = waitpid(childPid, &childStatus, 0);
+					
+					if(WIFEXITED(childStatus)){
+						fprintf(stdout, "Child %d exited normally with status %zu\n", childPid, childStatus);
+						fflush(stdout);
+					}else{
+						fprintf(stdout,"Child %d exited abnormally due to signal %zu\n", childPid, childStatus);
+						fflush(stdout);
+					}
 					break;
 			}
 				
@@ -133,13 +197,14 @@ int main(){
 		}
 
 		// trash collection before loop continues
-		userArgsCleanUp(Args.args, Args.amount_args);
-		if(Args.infile){
-			free(Args.infile);
-		}
-		if(Args.outfile){
-			free(Args.outfile);
-		}
+		for(int i=0; i < Args.amount_args; ++i) free(Args.args[i]);
+
+		//if(Args.infile){
+		//	free(Args.infile);
+		//}
+		//if(Args.outfile){
+		//	free(Args.outfile);
+		//}
 	}while(1);	
 	return 0;
 }
@@ -152,36 +217,36 @@ int main(){
  * */
 void addArgsToStruct(char ** user_args, UserArgs *Args, int amount){
 
-	Args->infile = NULL;
-	Args->outfile = NULL;
+	//for(int i=0; i<Args->amount_args; ++i) Args->args[i] = NULL;
+	Args->infile[0] = '\0';
+	Args->outfile[0] = '\0';
 	Args->background = 0;
 	Args->amount_args = 0;
 
 	for(size_t i = 0; i<amount; ++i){
 		// malloc
-		if(user_args[i][0] != '>' && user_args[i][0] != '<' && strcmp(user_args[i], "&") != 0){
+		if(user_args[i][0] == '<' && user_args[i][1] == ' ' ){
+			// outfile
+			strncpy(Args->outfile, user_args[i]+2, strlen(user_args[i])-2);
+			Args->outfile[strlen(user_args[i])-2] = '\0';
+		}else if(user_args[i][0] == '>' && user_args[i][1] == ' '){
+			// infile
+			strncpy(Args->infile, user_args[i]+2, strlen(user_args[i])-2);
+			Args->infile[strlen(user_args[i])-2] = '\0';
+		}else if(strcmp(user_args[i], "&") != 0){
 			// add it
 			Args->args[Args->amount_args] = malloc(strlen(user_args[i])+1);
-			strcpy(Args->args[Args->amount_args], user_args[i]);
+			strncpy(Args->args[Args->amount_args], user_args[i], strlen(user_args[i]));
 			Args->args[Args->amount_args][strlen(user_args[i])] = '\0';
 			Args->amount_args++;
-		}else if(user_args[i][0] == '<' ){
-			// outfile
-			Args->outfile = malloc(strlen(user_args[i])+1);
-			strcpy(Args->outfile, user_args[i]);
-			Args->outfile[strlen(user_args[i])] = '\0';
-		}else if(user_args[i][0] == '>'){
-			// infile
-			Args->infile = malloc(strlen(user_args[i])+1);
-			strcpy(Args->infile, user_args[i]);
-			Args->infile[strlen(user_args[i])] = '\0';
 		}
 	}
+
 	if(strcmp(user_args[amount-1], "&") == 0){
 		// set background
 		Args->background = 1;
 	}
-
+	Args->args[Args->amount_args] = NULL;
 	Args->args[Args->amount_args] = '\0';
 	return;
 }
@@ -197,9 +262,11 @@ void displayArgs(UserArgs *Args){
 	fprintf(stdout, "=====PRINTING ARGS=====\n");
 	fflush(stdout);
 	// testing prints
-	for(int j = 0; j<Args->amount_args; ++j){
-		fprintf(stdout,"arg: %s\n", Args->args[j]);
-		fflush(stdout);
+	if(Args->args){
+		for(int j = 0; j<Args->amount_args; ++j){
+			fprintf(stdout,"arg: %s\n", Args->args[j]);
+			fflush(stdout);
+		}
 	}
 	if(Args->infile){
 		fprintf(stdout, "infile: %s\n", Args->infile);
